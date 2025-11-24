f65762214bddbc12569dd1b699598e8e673bca9b
flaskyian <flasky.lrdnmgnspq@gmail.com>
2025-11-24 16:47:55 +0800

    add: Part 1 - GPIO Basics

---
+++ Part 1 - GPIO Basics/main.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+LED_PIN = 17
+
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        print("LED ON")
+        time.sleep(1)
+
+        GPIO.output(LED_PIN, GPIO.LOW)
+        print("LED OFF")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Program stopped by user.")
+
+finally:
+    GPIO.cleanup()
